c76e68e5f543440a5daa1def4f0504707b58f4be
vindayunita <vindasari7684@gmail.com>
2024-09-27 09:06:12 +0700

    Vinda_dataKeanggotaanKafe

---
+++ Siakad24.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Siakad24 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String nama, nim;
+        char kelas;
+        byte absen;
+        double nilaiKuis, nilaiTugas, nilaiUjian, nilaiAkhir;
+
+        System.out.print("Masukan nama: ");
+        nama = sc.nextLine();
+        System.out.print("Masukkan NIM: ");
+        nim = sc.nextLine();
+        System.out.print("Masukkan kelas: ");
+        kelas = sc.nextLine().charAt(0);
+        System.out.print("Masukkan nomor absen: ");
+        absen = sc.nextByte();
+
+        System.out.print("Masukkan nilai kuis: ");
+        nilaiKuis = sc.nextDouble();
+        System.out.print("Masukkan nilai tugas: ");
+        nilaiTugas = sc.nextDouble();
+        System.out.print("Masukkan nilai ujian: ");
+        nilaiUjian = sc.nextDouble();
+
+        nilaiAkhir = (nilaiKuis + nilaiTugas + nilaiUjian) / 3;
+
+        System.out.print("Nama; " + nama + " NIM: " + nim);
+        System.out.print("Kelas: " + kelas + "Absen: " + absen);
+        System.out.print("Nilai Akhir: " + nilaiAkhir);
+    }
+}
